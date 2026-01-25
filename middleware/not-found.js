2f387b76640cc210728dd61773e9101ad51dd83a
ub-victor <victoireushindi371@gmail.com>
2026-01-25 18:06:15 +0200

    Initial commit

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,12 +1,3 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .send('Something went wrong try again later')
-}
+const notFound = (req, res)=> res.status(404).send('Route  does not exit')
 
-module.exports = errorHandlerMiddleware
+module.exports = notFound;
