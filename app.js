908560ebd601b41b2036fe25c2e8a48b7536b454
ub-victor <victoireushindi371@gmail.com>
2026-01-25 16:38:06 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
-const app = express
+const app = express();
 
 
 const notFoundMiddleware = require('./middleware/not-found')
