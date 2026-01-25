94a9017f77ee7e69f55f63d7ba4b44dff54c08c0
ub-victor <victoireushindi371@gmail.com>
2026-01-25 16:33:24 +0200

    Initial commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const port = process.env.PORT || 3000
 
 const start = async()=> {
     try{
+        app.listen(port, console.log(`Serverf is listening on port ${port}`))
 
     }catch(error){
         console.log(error)
