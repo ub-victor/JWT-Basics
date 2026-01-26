8345cd80de3d7e0f5740d5621596dc5eda395bf8
ub-victor <victoireushindi371@gmail.com>
2026-01-26 16:40:09 +0200

    Initial commit

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,8 @@
 const login = async (req, res)=>{
     res.send('Fake Login/Register/Signup Route')
 }
+
+const dashboard = async (req, res)=>{
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status
+}
