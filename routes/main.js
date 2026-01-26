c39c32c1f71046cf6f98a404045ce91669f41d89
ub-victor <victoireushindi371@gmail.com>
2026-01-26 16:41:49 +0200

    Initial commit

--- routes/main.js
+++ routes/main.js
@@ -4,5 +4,5 @@ const login = async (req, res)=>{
 
 const dashboard = async (req, res)=>{
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello Doe`})
+    res.status(200).json({msg:`Hello Doe`, secret: `Here is your authorized data, your luck number is ${luckyNumber}`})
 }
