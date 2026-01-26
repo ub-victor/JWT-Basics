88c62b8f6aa55ff8fb551df089e90fec8f07be2e
ub-victor <victoireushindi371@gmail.com>
2026-01-26 16:44:39 +0200

    Initial commit

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,9 @@ const login = async (req, res)=>{
 
 const dashboard = async (req, res)=>{
     /*
-    
+    Math.floor(...): Rounds the resulting number down to the nearest whole integer.
+    Math.floor(0.1) becomes 0.
+    Math.floor(99.99) becomes 99. 
     */
     const luckyNumber = Math.floor(Math.random()*100);
     res.status(200).json({msg:`Hello Doe`, secret: `Here is your authorized data, your luck number is ${luckyNumber}`})
