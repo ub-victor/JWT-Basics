cf5822913b7836202538ad916048413c5e75f44b
ub-victor <victoireushindi371@gmail.com>
2026-01-26 17:35:28 +0200

    Initial commit

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,5 @@
 const express = require('express');
+const { dashboard } = require('../controllers/main');
 const router = express.Router();
 
+cont {login, dashboard} = require('../controllers/main')
