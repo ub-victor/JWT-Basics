d256afd6b2b9c9456ebf33cbaed703bfe0ac1bf1
ub-victor <victoireushindi371@gmail.com>
2026-03-22 18:54:55 +0200

    Stard the implementation of the backendx

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,9 @@
+// Check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+
+// setup authentication so only the request with JWT can access the dashboard
+
 const login = async (req, res)=>{
     res.send('Fake Login/Register/Signup Route')
 }
