bcaa2ff8d538a2d2cbf548febcf90ca1f59c73ca
ub-victor <victoireushindi371@gmail.com>
2026-03-23 13:05:13 +0200

    implementation of the logic

--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,8 +1,12 @@
+const { UnauthenticatedError } = require('.')
 const CustomAPIError = require('./custom-error')
 const  {StatusCodes} = require('http-status-codes')
 
 class unauthenticatedError extends CustomAPIError {
     constructor(message){
-        
+        super(message)
+        this.StatusCodes = StatusCodes.UNAUTHORIZED
     }
 } 
+
+module.exports = UnauthenticatedError;
