78fb553a9a0b37499676813e3c08e37985762e5d
ub-victor <victoireushindi371@gmail.com>
2026-03-23 13:43:23 +0200

    implementation of the logic

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,5 +1,5 @@
-const CustomAPIError = require ('./custom-error')
-const { StatusCodes} = require('http-status-codes')
+const CustomAPIError = require ('./custom-error');
+const { StatusCodes} = require('http-status-codes');
 class BadRequest extends CustomAPIError {
     constructor (message){
         super(message)
--- errors/custom-error.js
+++ errors/custom-error.js
@@ -1,5 +1,5 @@
 class CustomAPIError extends Error { // This create a class that extends the Build-in Error , which mean , is now have all the error Properties
-    constructor(message){ // I just initialize the construtor with 
+    constructor(message){ // I just initialize the construtor with message for the parent so  i can be able to set an msg
         super(message)  
 
     }
