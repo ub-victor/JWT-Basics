674208fcfa333c221f61d2de60bfebf5051d5ba2
ub-victor <victoireushindi371@gmail.com>
2026-03-23 14:31:53 +0200

    implementation of the logic

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,11 +1,11 @@
 const CustomAPIError = require ('./custom-error');
 const { StatusCodes } = require('http-status-codes'); // This package provides numerical HTTP status codes with Readabke name eg StatusCodes.BAD_REQUEST -> 400
 
-class BadRequest extends CustomAPIError {
+class BadRequestError extends CustomAPIError {
     constructor (message){
         super(message)
         this.statusCode = StatusCodes.BAD_REQUEST // 400 bad request
     }
 }
 
-module.exports = BadRequest
+module.exports = BadRequestError
