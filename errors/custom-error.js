0ab98381e79426b83fdda5add9b7a23b0898e15b
ub-victor <victoireushindi371@gmail.com>
2026-03-26 17:07:02 +0200

    Logic imprementation

--- errors/custom-error.js
+++ errors/custom-error.js
@@ -1,8 +1,6 @@
-class CustomAPIError extends Error { // This create a class that extends the Build-in Error , which mean , is now have all the error Properties
-    constructor(message){ // I just initialize the construtor with message for the parent so  i can be able to set an msg
-        super(message)  
-
+class CustomAPIError extends Error {
+    constructor(message, statusCode){
+        super(message)
+        this.statusCode
     }
 }
-
-module.exports = CustomAPIError;
