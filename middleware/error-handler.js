8acb7d40a57194527fbd5d2cfadc23ef91f30a99
ub-victor <victoireushindi371@gmail.com>
2026-04-08 00:25:34 +0200

    Logic setting up

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
-const errorHandlerMiddleware = (err, req, res, next) => {
+const errorHandlerMiddleware = (err, req, res, next) => { 
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
